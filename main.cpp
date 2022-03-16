5ead30f3e35caa51a4135b0ff022602257e6c2b5
Damian2057 <ddamian2057@gmail.com>
2022-03-16 09:46:35 +0100

    printl add

--- main.cpp
+++ main.cpp
@@ -7,12 +7,18 @@
 #include "include/CodeWord.h"
 #include "include/DecodeWord.h"
 
+void printl(char text) {
+    std::cout<<text<<std::endl;
+}
+
+
 int main() {
 
     auto Code = std::make_shared<CodeWord>();
     auto Decode = std::make_shared<DecodeWord>();
     //std::cout<<Code->code('a',10);
-    std::cout<<Decode->decode("011000010110001011");
+    printl(Decode->decode("011000010110001011"));
+   // std::cout<<Decode->decode("011000010110001011");
     system("pause");
     return 0;
 }
